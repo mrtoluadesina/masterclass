8da383056396c9d9fb0f269e45ea01002ce8f0e9
Tolu Adesina <toluadesina@Tolus-MacBook-Pro.local>
2018-10-05 15:31:04 +0100

    reworked modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,30 +90,18 @@ $(document).ready(function(){
       $(".show-course").attr('src',srcimg);
     });
 
-    // Get the modal
-    var modal = document.getElementById('myModal');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-
+    // Modal
+    $(document).ready(function(){
+        $('#myBtn').click(function(){
+            $('#myModal').show();
+        });
+        $('span.close').click(function(){
+            $('#myModal').hide();
+        })
+        $(window).click(function(e){
+            if ($('#myModal').is(e.target)) {
+                $('#myModal').hide();
+            }
+        })
+    })
  });
